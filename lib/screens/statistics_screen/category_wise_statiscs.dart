449b78ad1d576b540a8e2f9ff2197dfcfea5f094
Joyal Joseph <joyaljosephnote@gmail.com>
2023-08-29 13:15:11 +0530

    category page update

--- lib/screens/statistics_screen/category_wise_statiscs.dart
+++ lib/screens/statistics_screen/category_wise_statiscs.dart
@@ -5,6 +5,7 @@ import 'package:fin_trackr/db/functions/currency_function.dart';
 import 'package:fin_trackr/db/functions/transaction_function.dart';
 import 'package:fin_trackr/models/statistics/statistics_model.db.dart';
 import 'package:fin_trackr/models/transactions/transaction_model_db.dart';
+import 'package:fin_trackr/screens/statistics_screen/widget/category_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:ionicons/ionicons.dart';
@@ -58,13 +59,13 @@ class _CategoryWiseStatisticsState extends State<CategoryWiseStatistics> {
         actions: [
           IconButton(
               onPressed: () {
-                showPopupMenu1();
+                CategoryFilter().showPopupMenu1(context);
               },
               icon: const Icon(Icons.sort_rounded)),
           IconButton(
               alignment: Alignment.centerLeft,
               onPressed: () {
-                showPopupMenu2();
+                CategoryFilter().showPopupMenu2(context);
               },
               icon: const Icon(Ionicons.calendar_outline, size: 22)),
         ],
@@ -165,7 +166,8 @@ class _CategoryWiseStatisticsState extends State<CategoryWiseStatistics> {
             ValueListenableBuilder(
               valueListenable: TransactionDB.instance.transactionListNotifier,
               builder: (context, value, child) {
-                Map<String, List<TransactionModel>> model = sortByDate(value);
+                Map<String, List<TransactionModel>> model =
+                    CategoryFilter().sortByDate(value);
                 String? endDate;
                 String? startDate;
                 if (value.isNotEmpty) {
@@ -398,137 +400,5 @@ class _CategoryWiseStatisticsState extends State<CategoryWiseStatistics> {
         ),
       ),
     );
-  }
-
-  void showPopupMenu1() async {
-    await showMenu(
-      color: AppColor.ftAppBarColor,
-      context: context,
-      position: const RelativeRect.fromLTRB(100, 80, 10, 10),
-      items: [
-        PopupMenuItem(
-            onTap: () {
-              TransactionDB.instance.filter('Income');
-              TransactionDB.instance.filterByDate(
-                  DateTime(DateTime.now().year, DateTime.now().month,
-                      DateTime.now().day - 30),
-                  DateTime(DateTime.now().year, DateTime.now().month,
-                      DateTime.now().day));
-            },
-            child: const Text(
-              'Income',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: AppColor.ftTextSecondayColor),
-            )),
-        PopupMenuItem(
-            onTap: () {
-              TransactionDB.instance.filter('Expense');
-              TransactionDB.instance.filterByDate(
-                  DateTime(DateTime.now().year, DateTime.now().month,
-                      DateTime.now().day - 30),
-                  DateTime(DateTime.now().year, DateTime.now().month,
-                      DateTime.now().day));
-            },
-            child: const Text(
-              'Expense',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: AppColor.ftTextSecondayColor),
-            )),
-      ],
-      elevation: 8.0,
-    );
-  }
-
-  void showPopupMenu2() async {
-    await showMenu(
-      color: AppColor.ftAppBarColor,
-      context: context,
-      position: const RelativeRect.fromLTRB(100, 80, 10, 10),
-      items: [
-        PopupMenuItem(
-            onTap: () {
-              TransactionDB.instance.filterByDate(
-                  DateTime(DateTime.now().year, DateTime.now().month,
-                      DateTime.now().day - 30),
-                  DateTime(DateTime.now().year, DateTime.now().month,
-                      DateTime.now().day));
-            },
-            child: const Text(
-              'All',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: AppColor.ftTextSecondayColor),
-            )),
-        PopupMenuItem(
-            onTap: () {
-              TransactionDB.instance.filterDataByDate('today');
-            },
-            child: const Text(
-              'Today',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: AppColor.ftTextSecondayColor),
-            )),
-        PopupMenuItem(
-            onTap: () {
-              TransactionDB.instance.filterDataByDate('yesterday');
-            },
-            child: const Text(
-              'Yesterday',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: AppColor.ftTextSecondayColor),
-            )),
-        PopupMenuItem(
-            onTap: () {
-              TransactionDB.instance.filterDataByDate('last week');
-            },
-            child: const Text(
-              'Last Week',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: AppColor.ftTextSecondayColor),
-            )),
-        PopupMenuItem(
-          onTap: () async {
-            var daterange = DateTimeRange(
-              start: DateTime(DateTime.now().year, DateTime.now().month,
-                  DateTime.now().day - 30),
-              end: DateTime(DateTime.now().year, DateTime.now().month,
-                  DateTime.now().day),
-            );
-            DateTimeRange? picked = await showDateRangePicker(
-                context: context,
-                firstDate: DateTime(DateTime.now().year - 1),
-                lastDate: DateTime.now(),
-                initialDateRange: daterange);
-            if (picked != null) {
-              TransactionDB.instance.filterByDate(picked.start, picked.end);
-            }
-          },
-          child: const Text(
-            'Custom Date',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: AppColor.ftTextSecondayColor),
-          ),
-        ),
-      ],
-      elevation: 8.0,
-    );
-  }
-
-  Map<String, List<TransactionModel>> sortByDate(
-      List<TransactionModel> models) {
-    Map<String, List<TransactionModel>> mapList = {};
-    for (TransactionModel model in models) {
-      if (!mapList.containsKey(model.date)) {
-        mapList[model.date] = [];
-      }
-      mapList[model.date]!.add(model);
-    }
-    return mapList;
   }
 }
---
+++ lib/screens/statistics_screen/widget/category_helper.dart
@@ -0,0 +1,138 @@
+import 'package:fin_trackr/constant/constant.dart';
+import 'package:fin_trackr/db/functions/transaction_function.dart';
+import 'package:fin_trackr/models/transactions/transaction_model_db.dart';
+import 'package:flutter/material.dart';
+
+class CategoryFilter {
+  void showPopupMenu1(BuildContext context) async {
+    await showMenu(
+      color: AppColor.ftAppBarColor,
+      context: context,
+      position: const RelativeRect.fromLTRB(100, 80, 10, 10),
+      items: [
+        PopupMenuItem(
+            onTap: () {
+              TransactionDB.instance.filter('Income');
+              TransactionDB.instance.filterByDate(
+                  DateTime(DateTime.now().year, DateTime.now().month,
+                      DateTime.now().day - 30),
+                  DateTime(DateTime.now().year, DateTime.now().month,
+                      DateTime.now().day));
+            },
+            child: const Text(
+              'Income',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: AppColor.ftTextSecondayColor),
+            )),
+        PopupMenuItem(
+            onTap: () {
+              TransactionDB.instance.filter('Expense');
+              TransactionDB.instance.filterByDate(
+                  DateTime(DateTime.now().year, DateTime.now().month,
+                      DateTime.now().day - 30),
+                  DateTime(DateTime.now().year, DateTime.now().month,
+                      DateTime.now().day));
+            },
+            child: const Text(
+              'Expense',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: AppColor.ftTextSecondayColor),
+            )),
+      ],
+      elevation: 8.0,
+    );
+  }
+
+  void showPopupMenu2(BuildContext context) async {
+    await showMenu(
+      color: AppColor.ftAppBarColor,
+      context: context,
+      position: const RelativeRect.fromLTRB(100, 80, 10, 10),
+      items: [
+        PopupMenuItem(
+            onTap: () {
+              TransactionDB.instance.filterByDate(
+                  DateTime(DateTime.now().year, DateTime.now().month,
+                      DateTime.now().day - 30),
+                  DateTime(DateTime.now().year, DateTime.now().month,
+                      DateTime.now().day));
+            },
+            child: const Text(
+              'All',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: AppColor.ftTextSecondayColor),
+            )),
+        PopupMenuItem(
+            onTap: () {
+              TransactionDB.instance.filterDataByDate('today');
+            },
+            child: const Text(
+              'Today',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: AppColor.ftTextSecondayColor),
+            )),
+        PopupMenuItem(
+            onTap: () {
+              TransactionDB.instance.filterDataByDate('yesterday');
+            },
+            child: const Text(
+              'Yesterday',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: AppColor.ftTextSecondayColor),
+            )),
+        PopupMenuItem(
+            onTap: () {
+              TransactionDB.instance.filterDataByDate('last week');
+            },
+            child: const Text(
+              'Last Week',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: AppColor.ftTextSecondayColor),
+            )),
+        PopupMenuItem(
+          onTap: () async {
+            var daterange = DateTimeRange(
+              start: DateTime(DateTime.now().year, DateTime.now().month,
+                  DateTime.now().day - 30),
+              end: DateTime(DateTime.now().year, DateTime.now().month,
+                  DateTime.now().day),
+            );
+            DateTimeRange? picked = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(DateTime.now().year - 1),
+                lastDate: DateTime.now(),
+                initialDateRange: daterange);
+            if (picked != null) {
+              TransactionDB.instance.filterByDate(picked.start, picked.end);
+            }
+          },
+          child: const Text(
+            'Custom Date',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: AppColor.ftTextSecondayColor),
+          ),
+        ),
+      ],
+      elevation: 8.0,
+    );
+  }
+
+  Map<String, List<TransactionModel>> sortByDate(
+      List<TransactionModel> models) {
+    Map<String, List<TransactionModel>> mapList = {};
+    for (TransactionModel model in models) {
+      if (!mapList.containsKey(model.date)) {
+        mapList[model.date] = [];
+      }
+      mapList[model.date]!.add(model);
+    }
+    return mapList;
+  }
+}
